2e9bf170999b98c50b5bb5aa821d302026f075e1
Vsevolod <fizik.vlk@gmail.com>
2024-08-26 16:54:50 +0200

    work on data

--- energy/exp_0/data_collection/metadata.py
+++
@@ -1,3 +0,0 @@
-locations = [
-    dict(name="Berlin", type="city", lat=52.520007, lon=13.404954, file="Berlin_52_520007_13_404954_66c71411bf8a6c0008d770ae.csv")
-]
---
+++ energy/exp_0/data_openweather/metadata.py
@@ -0,0 +1,4 @@
+locations = [
+    dict(name="Berlin", type="city", lat=52.520007, lon=13.404954, file="Berlin_52_520007_13_404954_66c71411bf8a6c0008d770ae.csv"),
+    dict(name="EnBWHoheSee", type="windfarm", lat=54.45001, lon=6.318156, file="EnBWHoheSee_a8b25d4858a8ce41df2900957d578916.csv"),
+]
